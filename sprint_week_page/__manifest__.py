48b493882f8bddb69516f608f83a4a6ea9ed25a8
TheoDelaere <102591086+TheoDelaere@users.noreply.github.com>
2025-03-31 15:26:00 +0200

    Branch creation

---
+++ sprint_week_page/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Burniaux Consulting Sprint Week',
+    'version': '18.0',
+    'author': 'Burniaux Consulting',
+    'category': 'Project',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/menu_sprint_week.xml',
+        'views/sprint_week_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
