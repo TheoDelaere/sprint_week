83cda7f441c651b342359894ea7513f02b03b74b
TheoDelaere <102591086+TheoDelaere@users.noreply.github.com>
2025-04-07 10:11:00 +0200

    Affichage de tous les sates en permanence.

--- bc_sprint/models/project_task.py
+++ bc_sprint/models/project_task.py
@@ -2,11 +2,13 @@ from odoo import models, fields, api, _
 
 class ProjectTask(models.Model):
     _inherit = "project.task"
+    _order = "name desc"
 
     sprint_id = fields.Many2one('sprint', string="Sprint")
-    state_id = fields.Many2one('sprint.state', string="State from Sprint")
+    state_id = fields.Many2one('sprint.state', string="State from Sprint", group_expand='_group_expand_states')
 
     @api.model
-    def _group_expand_states(self, states, domain, order):
-        return self.env['project.state'].search([], order='sequence').ids
+    def _group_expand_states(self, states, domain):
+        return self.env['sprint.state'].search(domain=domain)
+
 
