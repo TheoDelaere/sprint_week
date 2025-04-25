e5fd7015f7d72cd4649741bfb397f4c20a0c6409
SoyMath <soyeur.mathieu@gmail.com>
2025-04-25 11:39:57 +0200

    commented obselete function

--- bc_sprint/models/project_task.py
+++ bc_sprint/models/project_task.py
@@ -45,23 +45,23 @@ class ProjectTask(models.Model):
             return self.env['sprint'].search([])
 
     def compute_something(self):
-        week1_sprint = self.env['sprint'].search([('name', '=', 'Week 1')])
-        week2_sprint = self.env['sprint'].search([('name', '=', 'Week 2')])
-        week3_sprint = self.env['sprint'].search([('name', '=', 'Week 3')])
-        sprint_test = self.env['sprint'].search([('name', '=', date.today().strftime("%d-%m-%Y"))])
-        if sprint_test.name != date.today().strftime("%d-%m-%Y"):
-            new_sprint = self.env['sprint'].create({
-                'name': date.today().strftime("%d-%m-%Y"),
-            })
-        else :
-            new_sprint = sprint_test
-        
-        for task in self:
-            if task.sprint_id == week1_sprint:
-                task.sprint_id = new_sprint.id
-            elif task.sprint_id == week2_sprint:
-                task.sprint_id = week1_sprint.id
-            elif task.sprint_id == week3_sprint:
-                task.sprint_id = week2_sprint.id
-            print(task.name, " = ", task.sprint_id.name)
+        # week1_sprint = self.env['sprint'].search([('name', '=', 'Week 1')])
+        # week2_sprint = self.env['sprint'].search([('name', '=', 'Week 2')])
+        # week3_sprint = self.env['sprint'].search([('name', '=', 'Week 3')])
+        # sprint_test = self.env['sprint'].search([('name', '=', date.today().strftime("%d-%m-%Y"))])
+        # if sprint_test.name != date.today().strftime("%d-%m-%Y"):
+        #     new_sprint = self.env['sprint'].create({
+        #         'name': date.today().strftime("%d-%m-%Y"),
+        #     })
+        # else :
+        #     new_sprint = sprint_test
+        # for task in self:
+        #     if task.sprint_id == week1_sprint:
+        #         task.sprint_id = new_sprint.id
+        #     elif task.sprint_id == week2_sprint:
+        #         task.sprint_id = week1_sprint.id
+        #     elif task.sprint_id == week3_sprint:
+        #         task.sprint_id = week2_sprint.id
+        #     print(task.name, " = ", task.sprint_id.name)
         return {"success": True}
+        
